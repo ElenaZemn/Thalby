235b09bdfa54950c749d5a193e06ebe74d1bc539
ElenaZemn <bevzelena666@gmail.com>
2023-05-01 14:41:00 +0300

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
         responsive:{
             0:{
                 items:1
+
             },
 
             1000:{
@@ -17,3 +18,21 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
+
